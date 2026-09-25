41b0d560db3a40d830d7f8b964c60980852ef701
Sardar balnour <balnour.sardar@gmail.com>
2026-09-25 11:40:37 +0330

    layout

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,10 @@
+function AboutLayout({ children }: LayoutProps<"/about">) {
+  return (
+    <div>
+      <h1>about layout</h1>
+      {children}
+    </div>
+  );
+}
+
+export default AboutLayout;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <header>Root Header</header>
+        {children}
+        <footer>Root Footer</footer>
+      </body>
     </html>
   );
 }
